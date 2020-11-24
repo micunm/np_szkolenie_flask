f0e2d4ad95ac83ce61f753f59c18ae9ded8f0e5d
Maciej Micun <maciej.micun@play.pl>
2020-11-24 13:04:26 +0100

    jinja template made during training

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ def index():
     return render_template('index.html', user_info=user_info)
 
 
-@app.route('/user/<name>/')
-def hello_from_kwargs(name):
-    return f'<h1>Hello string, {name}!</h1>'
+@app.route('/user/<name>/<int:amount>/')
+def hello_from_kwargs(name, amount):
+    return render_template('hello.html', name=name, amount=amount)
+    #return f'<h1>Hello string, {name} ({amount})!</h1>'
 
 
 if __name__ == '__main__':
