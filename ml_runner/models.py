2397c66c9e03e68e7e512bfb5c4a0e804b215592
Maciej Micun <maciej.micun@play.pl>
2020-11-26 10:25:32 +0100

    model withdatetime now

--- ml_runner/models.py
+++ ml_runner/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from ml_runner import db
 
 
@@ -7,3 +9,11 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    creation_date = db.Column(
+        db.DateTime, nullable=False,
+        default=lambda: datetime.now(tz=timezone.utc))
