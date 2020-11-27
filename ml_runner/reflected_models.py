093f347268c68e67eba1b7c9e4ec426d63956b48
Maciej Micun <maciej.micun@play.pl>
2020-11-27 12:30:15 +0100

    reflected models

--- ml_runner/reflected_models.py
+++ ml_runner/reflected_models.py
@@ -9,18 +9,18 @@ Base = automap_base()
 
 
 if os.environ.get('DO_NOT_REFLECT'):
-    ReflectedUser = None
-    ReflectedPost = None
+     ReflectedUser = None
+     ReflectedPost = None
 
-else:
-    # when generating db, reflecting db will cause errors
+ else:
+     # when generating db, reflecting db will cause errors
 
-    # class ReflectedUser(Base):
-    #     __tablename__ = 'user'
-    #     posts = relationship("post", collection_class=list, backref='user')
+    class ReflectedUser(Base):
+        __tablename__ = 'user'
+        posts = relationship("post", collection_class=list, backref='user')
 
     engine = db.engine
     Base.prepare(engine, reflect=True)
 
-    ReflectedUser = Base.classes.user
+    # ReflectedUser = Base.classes.user
     ReflectedPost = Base.classes.post
